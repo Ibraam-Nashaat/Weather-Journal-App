e24022ba09253d78003a80250ce9e5a04255fa98
Ibraam Nashaat <ibraam.nashaat2@gmail.com>
2022-09-26 21:30:32 +0200

    Set up the server and added API key

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-
+const baseUrl="https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey="&appid=e3aec68fd901b006998cdf08ebefcbc9&units=imperial";
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
